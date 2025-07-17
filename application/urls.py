0b56f2d9478885526579ebe1e760a70f82758eef
Samson NIYONIZERA <nisammy40@gmail.com>
2025-07-17 21:04:57 +0200

    added https ability

--- application/urls.py
+++ application/urls.py
@@ -11,7 +11,15 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 # Define the Swagger UI view
+from drf_yasg.generators import OpenAPISchemaGenerator
 
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
+    
 schema_view = get_schema_view(
    openapi.Info(
       title="Kiin Mailer API Documentation",
@@ -31,6 +39,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
+   generator_class=BothHttpAndHttpsSchemaGenerator,
    permission_classes=(permissions.AllowAny,),
 )
 
